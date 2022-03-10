44a508d0191fbe11445d6024fcc1c2f48269b8bc
adinh <adinh254@gmail.com>
2022-03-09 21:07:01 -0800

    Initial Commit

---
+++ xcentium-login-application/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace xcentium_code_challenge.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Username is Required.")]
+        public String Username { get; set; }
+        [Required(ErrorMessage = "Password is Required.")]
+        public String Password { get; set; }
+        public String Name { get; set; }
+    }
+}
